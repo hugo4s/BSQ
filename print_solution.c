427c3582c79f110038eef85ea961517e66eff560
g24force <102439759+g24force@users.noreply.github.com>
2024-08-21 21:14:06 +0100

    WIP norm

--- print_solution.c
+++ print_solution.c
@@ -19,9 +19,8 @@ char	**populate_map(char **map, int rows, int cols, char obst_char, char square_
 	int	x;
 
 	square_last_pos = max_square_position(map, rows, cols, obst_char);
-	if (square_last_pos == NULL) {
+	if (square_last_pos == NULL)
 		return NULL;
-		}
 	square_first_pos[0] = square_last_pos[0] - square_last_pos[2] + 1;
 	square_first_pos[1] = square_last_pos[1] - square_last_pos[2] + 1;
 
@@ -57,11 +56,9 @@ void	print_solution(int i, char **map_path)
 	square_char = ft_get_char_full(map_path[i]);
 	num_of_rows = ft_get_number_lines(map_path[i]);
 	num_of_columns = ft_get_number_columns(map_path[i]);
-
 	fd = open(map_path[i], O_RDONLY);
 	if (fd >= 0)
 		map = ft_read_file(map_path[i]);
-
 	if (map != NULL)
 	{
 		if (populate_map(map, num_of_rows, num_of_columns, obst_char, square_char) != NULL)
